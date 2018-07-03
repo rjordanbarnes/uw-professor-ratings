e9682f4888a999ec32c62312460640368ec83eab
Min-Kai Hsu <mkhsu@uw.edu>
2018-07-03 11:17:13 +0800

    Added support for Bothell courses.

--- FindCoursesPage.js
+++ FindCoursesPage.js
@@ -1,3 +1,5 @@
+const uwbPrefixes = "B ACCT\0B BUS\0B BECN\0B BSKL\0ELCBUS\0B EDUC\0LEDE\0B ARAB\0B CORE\0B CHIN\0BJAPAN\0BKOREA\0B LEAD\0B MATH\0B SPAN\0B CUSP\0B WRIT\0BISAES\0BISCP\0BCWRIT\0BCULST\0BISCLA\0BEARTH\0BES\0BISGWS\0BISGST\0BISIA\0BISSTS\0BIS\0BISSKL\0BISLEP\0BISMCS\0BPOLST\0BISSEB\0B IMD\0B HLTH\0BHS\0B NURS\0STEM\0B BIO\0BCONSC\0CSS\0CSSSKL\0B CE\0B EE\0B ENGR\0B ME\0STMATH\0B IMD\0B CHEM\0B CLIM\0B PHYS\0BST";
+
 function onFindCoursesPageFound() {
     applyStyles();
 
@@ -110,7 +112,9 @@ function insertColumnData() {
             const instructorNameArray = instructor.split(" ");
             const instructorFirstName = instructorNameArray[0].substring(0, 6); // Only uses first 6 letters of the first name to increase hits.
             const instructorLastName = instructorNameArray[instructorNameArray.length - 1];
-            const rateMyProfessorAPI = "https://search.mtvnservices.com/typeahead/suggest/?solrformat=true&rows=1&q=" + instructorFirstName + " " + instructorLastName + "+AND+schoolid_s%3A1530&defType=edismax&qf=teacherfirstname_t%5E2000+teacherlastname_t%5E2000+teacherfullname_t%5E2000+autosuggest&bf=pow(total_number_of_ratings_i%2C2.1)&sort=total_number_of_ratings_i+desc&siteName=rmp&rows=1&start=0&fl=pk_id+teacherfirstname_t+teacherlastname_t+total_number_of_ratings_i+averageratingscore_rf+schoolid_s+averageeasyscore_rf&fq";
+
+            const schoolID = !uwbPrefixes.includes(courseCode.replace(/ \d*$/g, "")) ? 1530 : 4466;
+            const rateMyProfessorAPI = "https://search.mtvnservices.com/typeahead/suggest/?solrformat=true&rows=1&q=" + instructorFirstName + " " + instructorLastName + "+AND+schoolid_s%3A" + schoolID + "&defType=edismax&qf=teacherfirstname_t%5E2000+teacherlastname_t%5E2000+teacherfullname_t%5E2000+autosuggest&bf=pow(total_number_of_ratings_i%2C2.1)&sort=total_number_of_ratings_i+desc&siteName=rmp&rows=1&start=0&fl=pk_id+teacherfirstname_t+teacherlastname_t+total_number_of_ratings_i+averageratingscore_rf+schoolid_s+averageeasyscore_rf&fq";
 
             const request = $.ajax(rateMyProfessorAPI);
             ajaxRequests.push(request);
